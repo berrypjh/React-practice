e5db153f2217b0f217af4f79db0238835f5a01ef
Junhyuk Park <junhyeg08@gmail.com>
2022-08-18 16:19:58 +0900

    delay count

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import logo from './logo.svg';
+import { useCallback, useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const delay = useCallback((milliseconds) => {
+    return new Promise((resolve) => setTimeout(resolve, milliseconds))
+  }, [])
+
+  const handleOnClick = useCallback(async () => {
+    for (let i = 0; i < 10; i++) {
+      await delay(100);
+      setCount(prev => prev + 1);
+    }
+  }, []);
+
+  useEffect(() => {
+    handleOnClick()
+  }, [])
+  // }, [count])
+
   return (
     <div className="App">
-      얀녕하세요!
+      {count}
     </div>
   );
 }
